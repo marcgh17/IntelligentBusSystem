357a201100072d0ef1c5cac2e9438328586bfe17
marcgh17 <ghossein.marc@gmail.com>
2015-02-22 21:59:45 +0200

    Fixed Add User Page

--- IntelligentBusSystem/Controllers/ProfileController.cs
+++ IntelligentBusSystem/Controllers/ProfileController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using IntelligentBusSystem.Models;
+using System.IO;
 namespace IntelligentBusSystem.Controllers
 {
     public class ProfileController : Controller
@@ -14,7 +15,7 @@ namespace IntelligentBusSystem.Controllers
         {
             using (var context = new IntelligentBusSystemEntities())
             {
-                var us=User.Identity.Name;
+                var us = User.Identity.Name;
                 var user = context.SUsers.Find(us);
                 return View((SUser)user);
             }
@@ -22,6 +23,57 @@ namespace IntelligentBusSystem.Controllers
         public ActionResult AddUser()
         {
             return View();
+
         }
-	}
+        [HttpPost]
+        public ActionResult AddUser(SignUpViewModel model, HttpPostedFileBase uploadFile)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using (var context = new IntelligentBusSystemEntities())
+                {
+                    SUser newuser = new SUser();
+                    SUser olduser = context.SUsers.Find(model.UserName);
+                    if (olduser == null)
+                    {
+                        newuser.SUserID = model.UserName;
+                        newuser.SUserPassword = model.Password;
+                        newuser.SUserFirstName = model.FirstName;
+                        newuser.SUserLastName = model.LastName;
+                        newuser.SUserRole = model.Role;
+                        if (uploadFile != null)
+                        {
+                            string databasePath = "/img/Users/" + Path.GetFileName(uploadFile.FileName);
+                            string filePath = HttpContext.Server.MapPath(databasePath);
+                            uploadFile.SaveAs(filePath);
+                            newuser.SUserPhoto = databasePath;
+
+                        }
+                        else
+                        {
+                            string databasePath = "/img/Users/nophoto.png";
+                            newuser.SUserPhoto = databasePath;
+
+                        }
+                        context.SUsers.Add(newuser);
+                        context.SaveChanges();
+                        //UserAdded
+                        //Put After Add Code Here
+                        return View();
+
+                    }
+                        
+                    else
+                    {
+                        ModelState.AddModelError("", "Username already exists!");
+                    }
+
+                }
+            }
+
+         
+            return View(model);
+        }
+    }
 }
---
+++ IntelligentBusSystem/Models/ProfileViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace IntelligentBusSystem.Models
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage = "Username is Required")]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation must match!")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required(ErrorMessage = "First name is Required")]
+        [Display(Name = "Last Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is Required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Role is Required")]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+
+        [Display(Name = "Photo")]
+        public string Photo { get; set; }
+
+    }
+}
