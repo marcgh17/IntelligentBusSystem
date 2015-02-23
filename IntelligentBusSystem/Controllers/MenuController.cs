1ebba0d43657d015d5055796a4fefba0f06fc587
marcgh17 <ghossein.marc@gmail.com>
2015-02-23 22:36:40 +0200

    added menu controller to commit

---
+++ IntelligentBusSystem/Controllers/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntelligentBusSystem.Models;
+
+namespace IntelligentBusSystem.Controllers
+{
+    [AllowAnonymous]
+    public class MenuController : Controller
+    {
+        //
+        // GET: /Menu/
+        public ActionResult Index()
+        {
+            if(!Request.IsAuthenticated)
+            return PartialView("_Menu",GetMenuItems("A"));
+            else
+                return PartialView("_Menu", GetMenuItems("A"));
+        }
+
+
+        public List<CustomMenuItem> GetMenuItems(string role)
+        {
+            List<CustomMenuItem> menus=new List<CustomMenuItem>();
+            using (var context = new IntelligentBusSystemEntities())
+            {
+                List<WebAppMenu> dbMainMenus = context.WebAppMenus.Where(c=>c.WebAppMenuParent==0).OrderBy(c => c.WebAppMenuPosition).ToList();
+                foreach (var dbMM in dbMainMenus)
+                {
+                    CustomMenuItem mm = new CustomMenuItem();
+                    mm.Menu = dbMM;
+                    mm.subMenus = context.WebAppMenus.Where(c => c.WebAppMenuParent==dbMM.WebAppMenuID).OrderBy(c => c.WebAppMenuPosition).ToList();
+                    menus.Add(mm);
+
+                }
+            }
+            return menus;
+
+        }
+	}
+}
---
+++ IntelligentBusSystem/Controllers/SchoolInfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntelligentBusSystem.Controllers
+{
+    public class SchoolInfoController : Controller
+    {
+        //
+        // GET: /SchoolInfo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
