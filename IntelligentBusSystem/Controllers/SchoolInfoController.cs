17332953d871d54a86bc52d2e48f83044551a881
marcgh17 <ghossein.marc@gmail.com>
2015-02-25 14:46:07 +0200

    Fixed Thumb

--- IntelligentBusSystem/Controllers/SchoolInfoController.cs
+++ IntelligentBusSystem/Controllers/SchoolInfoController.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IntelligentBusSystem.Models;
 
 namespace IntelligentBusSystem.Controllers
-{
+{[Authorize]
     public class SchoolInfoController : Controller
     {
         //
         // GET: /SchoolInfo/
         public ActionResult Index()
         {
-            return View();
+            if(!UserAccountsController.checkIfAdmin(User.Identity.Name)) return Redirect("/");
+            using (var context = new IntelligentBusSystemEntities())
+            {
+
+                School s = context.Schools.ToList().First();
+                if (s != null)
+                    return View(s);
+                else
+                    return View();
+            }
+           
         }
+[AllowAnonymous]
+        public ActionResult Header()
+        {
+            using (var context = new IntelligentBusSystemEntities())
+            {
+
+                School s = context.Schools.ToList().First();
+                if (s != null)
+                    return PartialView("SchoolHeader", s);
+                else
+                    return PartialView("SchoolHeader");
+            }
+
+        }
+
+
 	}
+
+
 }
