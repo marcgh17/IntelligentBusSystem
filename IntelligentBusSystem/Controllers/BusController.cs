610750b8bebb31fafc3be3e95be1b3e2f1250141
marcgh17 <ghossein.marc@gmail.com>
2015-02-28 23:08:16 +0200

    + bus monitor

---
+++ IntelligentBusSystem/Controllers/BusController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntelligentBusSystem.Models;
+
+namespace IntelligentBusSystem.Controllers
+{
+    public class BusController : Controller
+    {
+        //
+        // GET: /Bus/
+        public ActionResult MonitorBuses()
+        {
+            using (var context = new SerializerContext())
+            {
+                List<Bus> buses= context.Buses.ToList();
+                return View(buses);
+            }
+            
+        }
+	}
+}
